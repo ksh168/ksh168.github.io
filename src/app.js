9a7a30cd583180f3836ac5255817a2d569e6a48d
ksh168 <mailkunalsharma99@gmail.com>
2025-07-17 14:09:51 +0000

    updated

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Navbar from './components/navbar';
 import { navigationLinks } from './config/navigation';
 import ScrollButton from './pages/scrollButton';
@@ -12,6 +12,8 @@ function App() {
         {navigationLinks.map(({ path, component: Component }) => (
           <Route key={path} path={path} element={<Component />} />
         ))}
+        {/* Catch-all route to redirect to homepage */}
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
       <ScrollButton />
     </Router>
--- src/config/navigation.js
+++ src/config/navigation.js
@@ -1,6 +1,6 @@
 import Home from '../pages/home';
 import Education from '../pages/education';
-import Experience from '../pages/experience';
+// import Experience from '../pages/experience';
 import Projects from '../pages/projects';
 import Contact from '../pages/contact';
 import Publications from '../pages/publications';
@@ -9,7 +9,7 @@ export const navigationLinks = [
   { path: '/', label: 'Home', component: Home },
   { path: '/education', label: 'Education', component: Education },
   { path: '/publications', label: 'Publications', component: Publications },
-  { path: '/experience', label: 'Experience', component: Experience },
+  // { path: '/experience', label: 'Experience', component: Experience },
   { path: '/projects', label: 'Projects', component: Projects },
   { path: '/contact', label: 'Contact', component: Contact },
 ];
